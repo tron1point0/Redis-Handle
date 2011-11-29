9fe45d9e83f0948c86776d3cb6304fe2b53dc8f6
Traian Nedelea <tron1point0@gmail.com>
2011-11-29 14:20:10 -0600

    Fixed POD

--- lib/Redis/Handle.pm
+++ lib/Redis/Handle.pm
@@ -10,10 +10,36 @@ use AnyEvent::Redis;
 
 # use Data::Dump qw(pp);
 
+=head1 NAME
+
+Redis::Handle - A filehandle tie for a Redis queue
+
+=head1 SYNOPSYS
+
+    tie *REDIS, 'Redis::Handle';
+    print REDIS "Foo bar baz\n";
+    print while <REDIS>;        # Prints "Foo bar baz\n"
+    
+    print REDIS "Foo", "Bar";
+    my @baz = <REDIS>           # @baz is now ("Foo","Bar")
+    
+    print REDIS "Foo", "Bar";
+    print <REDIS>;              # Prints "Foo"
+
+=head1 DESCRIPTION
+
+C<Redis::Handle> implements a tie interface to a Redis queue so that you can
+treat said queue as a filehandle. Pushing to the front of the queue is the same
+as a C<print>, and popping off the end of the queue is like a C<readline>.
+
+=cut
+
 {
-	my $timeout = 30;   # For BLPOPs, in seconds
-	my $redis;          # We want only a single Redis connection
-	my %redis;          # Connection information
+    my $timeout = 30;   # For BLPOPs, in seconds
+    my $redis;          # We want only a single Redis connection
+    my %redis;          # Connection information
+
+=head1 METHODS
 
 =head2 TIEHANDLE
 
@@ -21,18 +47,18 @@ Ties the filehandle to the clientId in Redis.
 
 =head3 Usage
 
-	tie *CLIENT, "Redis::MessageQueue", $clientId;
+    tie *CLIENT, "Redis::Handle", $clientId;
 
-    tie *CLIENT, 'Redis::MessageQueue', $clientId,
+    tie *CLIENT, 'Redis::Handle', $clientId,
         timeout => 100,
         host => 'example.com',
         port => 5800;
 
 =cut
 
-	sub TIEHANDLE {
-		my ($class,$clientId) = (+shift,+shift);
-		%redis = @_;
+    sub TIEHANDLE {
+        my ($class,$clientId) = (+shift,+shift);
+        %redis = @_;
         $redis ||= Redis->new(%redis);
 
         if ($redis{timeout}) {
@@ -40,8 +66,8 @@ Ties the filehandle to the clientId in Redis.
             delete $redis{timeout};
         }
 
-		bless \$clientId, $class;
-	}
+        bless \$clientId, $class;
+    }
 
 =head2 PRINT
 
@@ -51,19 +77,19 @@ including asynchronously pushing _other_ messages.
 
 =head3 Usage
 
-	print CLIENT { text => "foo", from => "bar" };
-	print CLIENT { text => "foo" }, { text => "bar" }, "System message";
+    print CLIENT nfreeze({ text => "foo", from => "bar" });
+    print CLIENT nfreeze({ text => "foo" }), nfreeze({ text => "bar" }), "System message";
 
 =cut
-	sub PRINT {
-		my $this = shift;
+    sub PRINT {
+        my $this = shift;
         $redis->ping or $redis = Redis->new(%redis);
         foreach (@_) {
             $redis->lpush($$this, $_) or
                 croak qq{Failed to push message [$_] to [$$this]: $!};
         }
         return 1;
-	}
+    }
 
 =head2 READLINE
 
@@ -75,26 +101,26 @@ to deal with the blocking operation.
 
 =head3 Usage
 
-	my $message = <CLIENT>;     # Reads only the next one
-	my @messages = <CLIENT>;    # Flushes the message queue into @messages
+    my $message = <CLIENT>;     # Reads only the next one
+    my @messages = <CLIENT>;    # Flushes the message queue into @messages
 
 =cut
-	sub READLINE {
-		my $this = shift;
+    sub READLINE {
+        my $this = shift;
         $redis->ping or $redis = Redis->new(%redis);
-		my $r = AnyEvent::Redis->new(%redis) or
+        my $r = AnyEvent::Redis->new(%redis) or
             croak qq(Couldn't create AnyEvent::Redis connection to [@{[%redis]}]: $!);
-		my $message;
-		until ($message) {
+        my $message;
+        until ($message) {
             my $cv = $r->brpop($$this, $timeout, sub {
-			    $message = $_[0][1];
-		    }) or croak qq{Couldn't BRPOP from [$$this]: $!};
-		    $cv->recv;
+                $message = $_[0][1];
+            }) or croak qq{Couldn't BRPOP from [$$this]: $!};
+            $cv->recv;
         }
         $r->quit; undef $r;
-		return $message unless wantarray;
-		return ($message, _flush($this));
-	}
+        return $message unless wantarray;
+        return ($message, _flush($this));
+    }
 
 =for READLINE
 
@@ -104,15 +130,15 @@ If you pass C<_flush> a nonzero number, it will read that many messages. An
 explicit "0" means "read nothing", while an C<undef> means "read everything".
 
 =cut
-	sub _flush {
-		my ($this,$count) = @_;
+    sub _flush {
+        my ($this,$count) = @_;
         my @messages;
         while (my $m = $redis->rpop($$this)) {
             last if defined $count && --$count < 0;
             push @messages, $m;
         }
-		return @messages;
-	}
+        return @messages;
+    }
 
 =head2 EOF
 
@@ -130,10 +156,10 @@ of the queue or if the queue isn't open.
 Returns the length of the buffer.
 
 =cut
-	sub _len {
-		my $this = shift;
-		return $redis->llen($$this);
-	}
+    sub _len {
+        my $this = shift;
+        return $redis->llen($$this);
+    }
 
 =head2 poll_once
 
@@ -156,16 +182,16 @@ number of messages to return, and a callback as its arguments.
     }
 
 =cut
-	sub poll_once {
+    sub poll_once {
         my $fn = pop;
-		my ($this,$count) = @_;
-		my $r = AnyEvent::Redis->new(%redis);
-		$r->brpop($$this, $timeout, sub {
-			my $message = $_[0][1];
+        my ($this,$count) = @_;
+        my $r = AnyEvent::Redis->new(%redis);
+        $r->brpop($$this, $timeout, sub {
+            my $message = $_[0][1];
             $r->quit; undef $r;
             return $fn->($message, _flush($this,$count));
-		});
-	}
+        });
+    }
 
 =head2 CLOSE
 
